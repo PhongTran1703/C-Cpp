e3c8f001b0280641c7d225e94c12cd2d26d8488a
Phong Tran <thanhphongtran1703@gmail.com>
2025-11-24 15:14:41 +0000

    Practice C

--- src/main.c
+++ src/main.c
@@ -1,18 +1,26 @@
 #include<stdio.h>
-
+#include<string.h>
 int main(){
 
-	int age = 25;
-	float price = 19.99;
+	int age = 0;
+	float price = 0.0f;
 	double pi = 3.1415926535;
-	char currency = '$';
-	char name[] = "Phong Tran";
+	char currency = '\0';
+	char name[30] = "";
 
-	printf("%d\n", age);
-	printf("%.2f$\n", price);	
-	printf("%lf\n", pi);
-	printf("%c\n", currency);
-	printf("%s\n", name);
+	//printf("%d\n", age);
+	//printf("%.2f$\n", price);	
+	//printf("%lf\n", pi);
+	//printf("%c\n", currency);
+	//printf("%s\n", name);
+
+	
+	printf("Enter your full name: ");
+	fgets(name, sizeof(name), stdin);
+	name[strlen(name) - 1] = '\0';
+	printf("Your full name is %s\n", name);
+
+	printf("you are %d years old\n", age);
 
 	return 0;
 }
