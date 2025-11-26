a1decff0c9564042a69da841457f5adc0d6aa474
Phong Tran <thanhphongtran1703@gmail.com>
2025-11-26 14:07:47 +0000

    Read file

---
+++ src/readfile.c
@@ -0,0 +1,27 @@
+//READ a file
+
+#include<stdio.h>
+
+int main(){
+
+	FILE *pFile = fopen("text.txt", "r");
+
+	char buffer[1024] = {0};
+
+
+	if(pFile == NULL){
+		printf("Could not open file\n");
+		return 1;
+	}
+	
+	while(fgets(buffer, sizeof(buffer), pFile) != NULL){
+	
+		printf("%s", buffer);
+	
+	}
+
+	
+	fclose(pFile);
+
+	return 0;
+}
