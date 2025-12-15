e4b9e542648d03689ff79ec4d5c3b3881df11801
Phong Tran <thanhphongtran1703@gmail.com>
2025-12-15 14:45:22 +0000

    CMake

---
+++ CMake/build/CMakeFiles/Hellow.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hellow.dir/Hellow.cpp.o"
+  "CMakeFiles/Hellow.dir/Hellow.cpp.o.d"
+  "Hellow"
+  "Hellow.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hellow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
