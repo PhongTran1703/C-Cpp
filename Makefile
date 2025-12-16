c3df72b01b07770aad6e9d9c49dfa11290ba4c94
Phong Tran <thanhphongtran1703@gmail.com>
2025-12-16 00:18:10 +0000

    Working version of image viewer

--- Makefile
+++
@@ -1,9 +0,0 @@
- GCC = gcc
-  2 NVCC = nvcc
-  3 CUDA_FLAGS = -arch=sm_86
-  4
-  5
-  6 clean:
-  7     rm -f *.o
-~
-~
--- project/ImageViewer/src.c
+++ project/ImageViewer/src.c
@@ -5,31 +5,62 @@
 
 int main (){
 
-	const int width=900;
-	const int height=600;
+	FILE *in = stdin;
+	char *pthroway = calloc(1000, sizeof(char));
+	//Read first line (specifire P3 or P6 - ignore here)
+	fgets(pthroway ,1000, in);
+	//Read second line (comment)
+	fgets(pthroway ,1000, in);
+	//Read third line (dimensions: width / height)
+	char *pdimensions = calloc(1000, sizeof(char));
+	fgets(pdimensions ,1000, in);
+	//Read fourth line 
+	fgets(pthroway, 1000, in);
+	
+	free(pthroway);
+	
+	int width = -1;
+	int height = -1;
+	sscanf(pdimensions, "%d %d\n", &width, &height);
+	
+	free(pdimensions);
+	
+	printf("width=%d, height=%d\n", width, height);
 
 	SDL_Window *pwindow = SDL_CreateWindow("Image Viewer", 
 			SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0);
 	SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
 	
-	Uint8 r, g, b;
-	r = 0xFF;
-	g = 0x00;
-	b = 0x00;
-	Uint32 color = SDL_MapRGB(psurface->format, r, g, b);
-	
 	SDL_Rect pixel = (SDL_Rect){0,0,1,1};
-	
+	Uint32 color = 0;
 	for(int x = 0; x < width; x++ )
 	{
 		for(int y = 0; y < height; y++)
 		{
+			Uint8 r, g, b;
+			r = (char) getchar();
+			g = (char) getchar();
+			b = (char) getchar();
+			color = SDL_MapRGB(psurface->format, r, g, b);
 			pixel.x=x;
 			pixel.y=y;
 			SDL_FillRect(psurface, &pixel, color);
+
 		}	
 	}
 	SDL_UpdateWindowSurface(pwindow);
-	SDL_Delay(3000);
+
+	int app_running = 1;
+	while (app_running){
+		SDL_Event event;
+		while (SDL_PollEvent(&event)){
+		
+			if(event.type == SDL_QUIT) {
+			
+				app_running = 0;
+			}
+		}
+		SDL_Delay(300);
+	}
 
 }
