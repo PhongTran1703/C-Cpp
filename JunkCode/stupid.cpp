b08ecac1a4cf38449fca308694853845deb95300
Phong Tran <thanhphongtran1703@gmail.com>
2026-01-26 18:17:33 +0000

    junk

---
+++ JunkCode/stupid.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+int main(){
+
+	int n;
+
+	std::cout << "Enter a numbber: ";
+	std::cin >> n;
+
+
+	for(int i = 0; i < n; i++){
+	
+		std::cout << "*\n";
+	
+	}
+
+
+	return 0;
+}
