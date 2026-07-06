4bafd087fb1bd98a0c5b39c8e41644b451917533
Thanhphong03 <thanhphongtran1703@gmail.com>
2026-07-06 17:43:38 +0300

    first day coming back

---
+++ JunkCode/LearningProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");    
+    
+      int x = 5;
+      int y = 6;
+      Console.WriteLine(x + y);
+      float z = 4.45F;
+      Console.WriteLine(z);
+      Console.WriteLine("Enter your name: ");
+      String username = Console.ReadLine();
+      Console.WriteLine("Your name is: " + username);
+      int A = Math.Max(x,y);
+      Console.WriteLine(A);
+
+
+    }
+  }
+}
